74bc62539c96d8169f3b41d1408e2ea40c6b60fd
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-07 00:20:48 +0000

    all

---
+++ var.tf
@@ -0,0 +1,19 @@
+variable "engine" {}
+variable "engine_version" {}
+variable "num_cache_nodes" {}
+variable "node_type" {}
+variable "var.subnet_ids" {}
+
+variable "env" {}
+variable "tags" {}
+
+
+
+
+
+
+
+
+
+
+
