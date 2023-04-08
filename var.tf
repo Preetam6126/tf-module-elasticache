b19add1353d731acf6f4a5c7e62dd6221aa1a1ef
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-08 05:28:40 +0000

    hello

--- var.tf
+++ var.tf
@@ -2,7 +2,7 @@ variable "engine" {}
 variable "engine_version" {}
 variable "num_cache_nodes" {}
 variable "node_type" {}
-variable "var.subnet_ids" {}
+variable "subnet_ids" {}
 
 variable "env" {}
 variable "tags" {}
